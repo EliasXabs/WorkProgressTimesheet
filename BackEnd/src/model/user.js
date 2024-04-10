5e9db2378a473f7de1b07b096d2fed373b1ad366
EliasXabs <eliasxabs@gmail.com>
2024-04-10 23:08:05 +0300

    User Model Hashing

--- BackEnd/src/model/user.js
+++ BackEnd/src/model/user.js
@@ -2,26 +2,42 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../sequelize'); // Adjust the path as necessary
 
 const user = sequelize.define('USER', {
-  Id: {
+  id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  UserName: {
+  username: {
     type: DataTypes.STRING(255),
     unique: true
   },
-  FirstName: DataTypes.STRING(255),
-  LastName: DataTypes.STRING(255),
-  DateOfBirth: DataTypes.DATEONLY,
-  Joined_in: DataTypes.DATE,
-  Email: DataTypes.STRING(255),
-  ContactNumber: DataTypes.STRING(20),
-  Role: DataTypes.STRING(255),
-  Availability: DataTypes.STRING(255)
+  password: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+  firstname: DataTypes.STRING(255),
+  lastname: DataTypes.STRING(255),
+  dateofbirth: DataTypes.DATEONLY,
+  joined_in: DataTypes.DATE,
+  email: DataTypes.STRING(255),
+  contactnumber: DataTypes.STRING(20),
+  role: DataTypes.STRING(255),
+  availability: DataTypes.STRING(255)
 }, {
   timestamps: false,
-  tableName: 'USER'
+  tableName: 'USER',
+  hooks: {
+    beforeCreate: async (user) => {
+      const salt = await bcrypt.genSalt(10);
+      user.Password = await bcrypt.hash(user.Password, salt);
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('Password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.Password = await bcrypt.hash(user.Password, salt);
+      }
+    }
+  }  
 });
 
 module.exports = user;
