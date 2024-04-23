33fab6f1c73024daf70aabaaa720ed0e1bbb17a9
EliasXabs <eliasxabs@gmail.com>
2024-04-23 23:33:04 +0300

    Index bug fix

--- BackEnd/src/routes/index.js
+++ BackEnd/src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const authRouter = require('./authroutes');
+const addRouter = require('./notiroutes');
 
 const apiRouter = express.Router();
 apiRouter.use('/auth', authRouter);
+apiRouter.use('/create', addRouter);
 
 module.exports = {
+    addRouter,
     apiRouter,
     authRouter
 };
